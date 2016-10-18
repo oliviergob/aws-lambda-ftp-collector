c7c5d44bafda986ea2fca738f69fd98c24405445
Gobs <olivier.gob@gmail.com>
2016-10-18 23:26:16 +0100

    Revert "Unsuccesfull attempt with jsftp module"
    This reverts commit ba850d4fba1d7c2c1a6f1bf1d9b2fedca5885052.

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ console.log('Loading function');
 
 exports.handler = (event, context, callback) => {
 
-    var JSFtp = require("jsftp");
+    var Client = require('ftp');
     var fs = require('fs');
 
-    var remotePath = event.remotePath;
+    var path = event.path;
     var mask = event.mask;
     var config = event.config;
-    console.log('remotePath =', remotePath);
+    console.log('Path =', path);
     console.log('Mask =', mask);
 
     var re = new RegExp(mask);
@@ -20,60 +20,38 @@ exports.handler = (event, context, callback) => {
     const s3 = new AWS.S3();
 
 
-    var ftp = new JSFtp(config);
+    var c = new Client();
     var filesToDownLoad = [];
     var fileName;
 
     var myFunction = function(err, stream) {
       if (err) throw err;
-      console.dir("My Stream"+stream);
-      console.log("About to Download "+remotePath);
-      stream.pipe(fs.createWriteStream("/tmp/temp.test"));
+      console.log("About to Download "+fileName);
+      stream.pipe(fs.createWriteStream("temp.test"));
     }
 
-
-    ftp.ls(remotePath, function(err, res) {
-      res.forEach(function(file) {
-        fileName = file.name;
-        if (re.test(fileName))
-        {
-          console.log("Downloading "+fileName);
-          ftp.get(remotePath+"/"+fileName, '/tmp/'+fileName, function(hadErr) {
-            if (hadErr)
-              console.error('There was an error retrieving the file.'+hadErr);
-            else
-              console.log('File copied successfully!');
-          });
-        }
-        else {
-          console.log("Not Downloading "+fileName);
-        }
-      });
-    });
-
-  /*  c.on('ready', function() {
-      c.list(remotePath, function(err, list) {
+    c.on('ready', function() {
+      c.list(path, function(err, list) {
         if (err) throw err;
 
         for(var i in list){
-          fileName = remotePath+"/"+list[i].name;
-          filesToDownLoad.push({});
+          fileName = path+"/"+list[i].name;
           if (re.test(fileName))
           {
             console.log("Downloading "+fileName);
             c.get(fileName, myFunction);
           }
           else {
-            console.log("Not Downloading "+fileName);
+            console.log("Not Downloading "+path+"/"+fileName);
           }
         }
 
-        // c.end();
+        c.end();
       });
-    }); */
+    });
 
 
 
-  //  c.connect(config);
+    c.connect(config);
 
 };
